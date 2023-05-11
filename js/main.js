3616a8140f6294af390345e392d3a89fdae40236
Brian <bhoangster@live.ca>
2023-05-11 14:56:31 -0700

    made changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest() {
   const rapperName = document.querySelector('input').value
   try{
-    const response = await fetch(`https://easy-rap-api.vercel.app/api/${rapperName}`)
+    const response = await fetch(`https://rap-names-api-km3z.onrender.com/api/${rapperName}`)
     const data = await response.json()
 
     console.log(data)
